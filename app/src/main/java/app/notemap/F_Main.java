7c5e283b98322cb4451e2ce97ea553e27fcef070
MVDam <47767573t@iespoblenou.org>
2016-02-12 19:57:50 +0100

    añadido metodo para mostrar notas

--- app/src/main/java/app/notemap/F_Main.java
+++ app/src/main/java/app/notemap/F_Main.java
@@ -1,12 +1,17 @@
 package app.notemap;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.provider.ContactsContract;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -14,12 +19,24 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class F_Main extends Fragment {
+public class F_Main extends Fragment implements View.OnClickListener, View.OnLongClickListener{
+
+
+    public static final int CODIGO_SOLICITUD_RECONOCIMIENTO = 1;
+
+
+    private ImageButton btHablar;
+    private ImageButton btMapear;
+    private ImageButton btAnotar;
+
+    private EditText etAnotado;
+
 
     public F_Main() {
     }
@@ -29,8 +46,12 @@ public class F_Main extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.lay_f_main, container, false);
 
+        setInicialListeners(view);
+
         NoteMap app = (NoteMap)getActivity().getApplication();
         Firebase ref = app.getRef();
+
+
 
         ref.child("prueba").setValue("Mc Culo");
 
@@ -59,7 +80,66 @@ public class F_Main extends Fragment {
         return view;
     }
 
+
+
     public void msgToast (Context context, String tag, String msg){
         Toast.makeText(context, tag + ": " + msg, Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imbtVoz:
+                Intent intHabla = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intHabla.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+                try {
+                    startActivityForResult(intHabla, CODIGO_SOLICITUD_RECONOCIMIENTO);
+                    etAnotado.setText("");
+
+                } catch (ActivityNotFoundException a) {
+                    msgToast(getContext(), "ERROR: ", "reconocimiento de voz");
+                }
+                break;
+
+        }
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
+    }
+
+    public void setInicialListeners(View v){
+
+        btHablar = (ImageButton) v.findViewById(R.id.imbtVoz);
+        btHablar.setOnClickListener(this);
+        btMapear = (ImageButton) v.findViewById(R.id.imbtMap);
+        btHablar.setOnClickListener(this);
+        btAnotar = (ImageButton) v.findViewById(R.id.imbtNota);
+        btHablar.setOnClickListener(this);
+
+        etAnotado = (EditText) v.findViewById(R.id.edtxNota);
+
+    }
+
+    @Override
+    public void onActivityResult(int codigoSolicitud, int resultCode, Intent intent) {
+        super.onActivityResult(codigoSolicitud, resultCode, intent);
+
+        //Validamos si el codigo de solicitud es el mismo y si la voz captada no da error
+        switch (codigoSolicitud) {
+            case CODIGO_SOLICITUD_RECONOCIMIENTO: {
+                if (resultCode == A_Main.RESULT_OK && null != intent) {
+
+                    ArrayList<String> listReconocimeintoPorVoz = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    etAnotado.setText(listReconocimeintoPorVoz.get(0));   //Aqui escribe el resultado en el textView indicado
+                    msgToast(getContext(), "PRUEBA: ", "captacion de voz");
+                }
+                break;
+            }
+        }
+    }
+
 }

d594bd266cd8357394e3accc4b1001ef455d7145
MVDam <47767573t@iespoblenou.org>
2016-02-18 23:10:45 +0100

    - definiendo nuevo fragment del mapa (WIP)

--- app/src/main/java/app/notemap/F_Map.java
+++ app/src/main/java/app/notemap/F_Map.java
@@ -1,10 +1,13 @@
 package app.notemap;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,12 +21,20 @@ import com.firebase.client.ValueEventListener;
 
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer;
+import org.osmdroid.bonuspack.overlays.Marker;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.MinimapOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.OverlayManager;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
@@ -37,42 +48,44 @@ public class F_Map extends Fragment {
 
     Firebase ref;
 
-    MapController mapController;
-    MyLocationNewOverlay mlno;
+    private MapView map;
+    private IMapController iMapController;
+    private MyLocationNewOverlay mlno;
+    //private MinimapOverlay minimapOverlay;
+    private ScaleBarOverlay scaleBarOverlay;
+    private CompassOverlay compassOverlay;
+    private RadiusMarkerClusterer agrupacionNotaMarkers;
+    private int radioAgrupacion = 100;
+
+
     boolean hayControlZoom = true;
     boolean hayControlMultiTouch = true;
+    boolean hayCentradoInicial = true;
     boolean hayPrecisionOverlay = true;
     public ArrayList<Nota> notaLista;
     public LinkedList<OverlayItem> notaMarkersList;
+    private int zoomInicial;
 
-    public MapView map;
 
-    public F_Map() { }
+    public F_Map() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootMapView = inflater.inflate(R.layout.lay_f_map, container, false);
 
+        //Iniciales
         NoteMap app = (NoteMap) getActivity().getApplication();
         ref = app.getRef();
         map = (MapView) rootMapView.findViewById(R.id.mapView);
-        notaMarkersList = new LinkedList<>();
+        zoomInicial = 15;
 
-        ref.child("prueba").child("Notas").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot){
-                notasGetter(snapshot);
-            }
-            @Override
-            public void onCancelled(FirebaseError error) {msgToast(2, "DataListener de Firebase Cancelado");}
-        });
-
-
-        map = (MapView) rootMapView.findViewById(R.id.mapView);
-        map.setBuiltInZoomControls(hayControlZoom);
-        map.setMultiTouchControls(hayControlMultiTouch);
-        mapController = (MapController) map.getController();
+        //Configuraciones iniciales
+        setMap();
+        setZoom(zoomInicial);
+        setOverlays(getContext());
+        drawMarkers(getContext());
 
         Drawable marker = getResources().getDrawable(android.R.drawable.ic_input_get);
         int markerWidth = marker.getIntrinsicWidth();
@@ -81,7 +94,7 @@ public class F_Map extends Fragment {
 
         ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getContext());
 
-        mlno = new MyLocationNewOverlay(getContext(),new GpsMyLocationProvider(getContext()), map);
+        //mlno = new MyLocationNewOverlay(getContext(), new GpsMyLocationProvider(getContext()), map);
         mlno.enableMyLocation();
         //mlno.setDrawAccuracyEnabled(hayPrecisionOverlay);
 
@@ -94,8 +107,116 @@ public class F_Map extends Fragment {
         return rootMapView;
     }
 
-    public void msgToast (int numTag, String msg){
-        switch (numTag){
+
+    public void setMap(){
+        try {
+            //Determinamos los tiles del mapa para pintarlo
+
+            map.setTileSource(TileSourceFactory.MAPQUESTOSM);
+            map.setTilesScaledToDpi(true);
+
+            //Determinamos otras opciones del mapa
+            map.setBuiltInZoomControls(hayControlZoom);
+            map.setMultiTouchControls(hayControlMultiTouch);
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+            msgToast(2, "Dibujando el mapa inicial");
+        }
+    }
+
+    public void setZoom(int zoom){
+        try {
+            iMapController = map.getController();
+            iMapController.setZoom(zoom);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            iMapController.setZoom(zoomInicial);
+            msgToast (2, "Ajustando el Zoom al inicial");
+        }
+    }
+
+    public void setOverlays(Context context){
+        final DisplayMetrics metricaDelMapa = getResources().getDisplayMetrics();
+        mlno = new MyLocationNewOverlay(    context
+                ,new GpsMyLocationProvider(context)
+                ,map
+        );
+
+        mlno.enableMyLocation();
+        mlno.runOnFirstFix(new Runnable() {
+            @Override
+            public void run() { iMapController.animateTo(mlno.getMyLocation()); }
+        });
+
+        //Setear la escala del mapa
+        scaleBarOverlay = new ScaleBarOverlay(map);
+        scaleBarOverlay.setCentred(hayCentradoInicial);
+        scaleBarOverlay.setScaleBarOffset(metricaDelMapa.widthPixels / 2, 10);
+
+        //Setear la Brujula de Overlays
+        compassOverlay = new CompassOverlay( context
+                ,new InternalCompassOrientationProvider(context)
+                , map
+        );
+        compassOverlay.enableCompass();
+
+        //Añadir los setters al overlay
+        map.getOverlays().add(mlno);
+        map.getOverlays().add(this.scaleBarOverlay);
+        map.getOverlays().add(this.compassOverlay);
+    }
+
+
+    public void drawMarkers (Context context){
+        setAgrupacionMarkers(getContext(), radioAgrupacion);
+
+        final Firebase notas = ref.child("prueba").child("Notas");
+        notas.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot notasFireBase: dataSnapshot.getChildren()){
+
+                    Nota nota = notasFireBase.getValue(Nota.class);
+                    Marker notaMarker = new Marker(map);
+                    GeoPoint gp = new GeoPoint(nota.getLatitud() ,nota.getLongitud());
+
+                    notaMarker.setPosition(gp);
+
+                    ...
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+
+
+
+
+        });
+    }
+
+
+    public void setAgrupacionMarkers(Context context, int radio){
+        agrupacionNotaMarkers = new RadiusMarkerClusterer(context);
+        map.getOverlays().add(agrupacionNotaMarkers);
+
+        Drawable clusterIconoDrawable = getResources().getDrawable(android.R.drawable.ic_input_get);
+        int markerWidth = clusterIconoDrawable.getIntrinsicWidth();
+        int markerHeight = clusterIconoDrawable.getIntrinsicHeight();
+        clusterIconoDrawable.setBounds(0, markerHeight, markerWidth, 0);
+        Bitmap clusterIconBm = ((BitmapDrawable) clusterIconoDrawable).getBitmap();
+
+        agrupacionNotaMarkers.setIcon(clusterIconBm);
+        agrupacionNotaMarkers.setRadius(radio);
+    }
+
+
+    public void msgToast(int numTag, String msg) {
+        switch (numTag) {
             case 1:
                 Toast.makeText(getContext(), "PRUEBA: " + msg, Toast.LENGTH_LONG).show();
                 break;
